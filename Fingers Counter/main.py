a38ecf37406f1a4884e7d975efa04e7617c29e8b
Lore-byte <lmala06.tech@gmail.com>
2025-03-27 19:29:11 +0100

    init

---
+++ Fingers Counter/main.py
@@ -0,0 +1,63 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+def count_fingers(hand_landmarks):
+  
+    if hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP].y:
+        thumb = 1
+    else:
+        thumb = 0
+        
+    if hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y:
+        index = 1
+    else:
+        index = 0
+        
+    if hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y:
+        middle = 1
+    else:
+        middle = 0
+        
+    if hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y:
+        ring = 1
+    else:
+        ring = 0
+        
+    if hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y:
+        pinky = 1
+    else:
+        pinky = 0
+
+    total_fingers = thumb + index + middle + ring + pinky
+    return total_fingers
+
+with mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb_frame)
+
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                
+                num_fingers = count_fingers(hand_landmarks)
+                
+                cv2.putText(frame, f'Finger: {num_fingers}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        cv2.imshow("Finger Counting", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
